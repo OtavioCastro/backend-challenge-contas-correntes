1422e61120a121f0035b2f0a794a608c1d169d9a
Otávio de Castro <otavio.vn01@hotmail.com>
2024-01-15 21:01:28 -0300

    Commit inicial

---
+++ src/test/java/com/challange/backend/runthebank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challange.backend.runthebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
