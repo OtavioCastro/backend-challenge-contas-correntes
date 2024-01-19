6c649c50a624fb36e2c2cc4438a1b511927769f7
Otávio de Castro <otavio.vn01@hotmail.com>
2024-01-19 11:11:30 -0300

    Ajustes para documentação da API.

--- src/main/java/com/challenge/backend/runthebank/RunTheBankApplication.java
+++ src/main/java/com/challenge/backend/runthebank/RunTheBankApplication.java
@@ -1,11 +1,14 @@
 package com.challenge.backend.runthebank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info=@Info(title="Desafio: Run The Bank!"))
 public class RunTheBankApplication {
 
 	public static void main(String[] args) {
